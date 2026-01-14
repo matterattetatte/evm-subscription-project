402d7a1dd37778b8a49f2ef940c84b340a393b5b
Mattz <matias.marti@medieinstitutet.com>
2026-01-14 17:05:24 +0100

    set up vite

---
+++ frontend/playwright.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="node" />
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  timeout: 90_000,
+  fullyParallel: true,
+  reporter: [['html', { open: 'never' }], ['list']],
+  workers: 4,
+
+  use: {
+    launchOptions: {
+      devtools: true,
+    },
+    storageState: 'playwright/.auth/auth.json',
+    baseURL: 'http://localhost:5173',
+    headless: true, // false = you see everything, no flakes
+    viewport: { width: 1280, height: 800 },
+    screenshot: (process.env.PW_USE_SCREENSHOT as 'on' | 'off' | 'only-on-failure') || 'only-on-failure',
+    video: (process.env.PW_USE_VIDEO as 'on' | 'off' | 'retain-on-failure' | 'retain-on-failure') || 'retain-on-failure',
+    trace: (process.env.PW_USE_TRACE as 'on' | 'off' | 'retain-on-failure' | 'on-first-retry') || 'on-first-retry',
+
+    // THIS IS THE ONLY LINE THAT MATTERS
+  },
+  projects: [
+    {
+      name: 'e2e headless',
+      use: {
+        ...devices['Desktop Chrome'],
+        headless: true, // can be modified just in case
+        // plus anything else (viewport, baseURL, permissions, etc.)
+      },
+    },
+  ],
+
+  webServer: [
+    {
+      command: 'npm run dev:e2e',
+      url: 'http://localhost:5173',
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
+})
