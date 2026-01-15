401a4b0680d59aa46ed906176454683fd5519da2
Mattz <matias.marti@medieinstitutet.com>
2026-01-15 12:55:06 +0100

    set up routes to different views

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,19 @@ import './App.css';
 
 const Home = React.lazy(() => import('./views/Home'));
 const AdminDashboard = React.lazy(() => import('./views/AdminFlow/AdminDashboard'));
-const SingleSubscription = React.lazy(() => import('./views/SubsriberFlow/SingleSubscription'));
-const Subscriptions = React.lazy(() => import('./views/SubsriberFlow/Subscriptions'));
+const AdminSubscriptionHandling = React.lazy(() => import('./views/AdminFlow/AdminSubscriptionHandling'));
+const SingleSubscription = React.lazy(() => import('./views/SubscriberFlow/SingleSubscription'));
+const Subscriptions = React.lazy(() => import('./views/SubscriberFlow/Subscriptions'));
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/subscriptions/:id" element={<AdminSubscriptionHandling />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/subscriptions/:id" element={<SingleSubscription />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
