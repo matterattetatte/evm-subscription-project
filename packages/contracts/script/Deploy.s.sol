72476489aae3f108200b3a1eab0cb4ee6e6f2a1c
Mattz <matias.marti@medieinstitutet.com>
2026-01-20 17:28:40 +0100

    deploy to local

--- packages/contracts/script/Deploy.s.sol
+++ packages/contracts/script/Deploy.s.sol
@@ -3,21 +3,24 @@ pragma solidity 0.8.25;
 
 import "forge-std/Script.sol";
 import "../src/SubscriptionService.sol";
+import "../src/mocks/MockTimeOracle.sol";
 
-contract DeploySubscriptionService is Script {
+contract Deploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address keeper = vm.envAddress("KEEPER_ADDRESS");
-        address timeOracle = vm.envAddress("TIME_ORACLE_ADDRESS");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        SubscriptionService service = new SubscriptionService{ 
-            salt: bytes32(0) 
-        }(keeper, timeOracle);
+        MockTimeOracle timeOracle = new MockTimeOracle(0);
+
+        SubscriptionService service = new SubscriptionService{
+            salt: bytes32(0)
+        }(keeper, address(timeOracle));
 
         vm.stopBroadcast();
 
-        console.log("Deployed to:", address(service));
+        console.log("MockTimeOracle deployed to:", address(timeOracle));
+        console.log("SubscriptionService deployed to:", address(service));
     }
 }
